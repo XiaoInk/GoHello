3e0326672db9b043520107f1adb1cc4f43875c14
XiaoInk <xiaoqiangink@outlook.com>
2021-06-11 16:40:04 +0800

    简单的 HTTP Server

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+/*
+	Created by XiaoInk at 2021/06/11 16:11:00
+	GitHub: https://github.com/XiaoInk
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+type Server struct {
+	Port int
+	Word string
+}
+
+func main() {
+	server := &Server{}
+
+	flag.IntVar(&server.Port, "port", 8080, "服务端口")
+	flag.StringVar(&server.Word, "word", "Hello world.", "前端展示词")
+
+	flag.Parse()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(server.Word))
+	})
+
+	http.ListenAndServe(fmt.Sprintf(":%d", server.Port), nil)
+}
