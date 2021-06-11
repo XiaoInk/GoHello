2158989f7115d9b5c0cf58e517f8aae490cdd097
XiaoInk <xiaoqiangink@outlook.com>
2021-06-11 17:08:13 +0800

    初始化项目

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -26,6 +27,8 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(server.Word))
+
+		log.Println(r.RemoteAddr, r.Method, r.RequestURI)
 	})
 
 	http.ListenAndServe(fmt.Sprintf(":%d", server.Port), nil)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/XiaoInk/GoHello
+
+go 1.15
